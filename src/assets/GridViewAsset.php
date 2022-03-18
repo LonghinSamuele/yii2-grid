72c369659628637e853cf7ce6438978ad4e8c1ff
Samuele Longhin <longhin.samuele@outlook.com>
2022-03-18 16:33:39 +0100

    changing assets

--- src/assets/GridViewAsset.php
+++ src/assets/GridViewAsset.php
@@ -11,15 +11,12 @@ use yii\web\AssetBundle;
 
 class GridViewAsset extends AssetBundle
 {
-    /**
-     * @inheritdoc
-     */
-    public function init()
-    {
-        $this->depends = array_merge(["kartik\\grid\\GridViewAsset", "yii\\grid\\GridViewAsset"], $this->depends);
-        $this->setSourcePath(__DIR__ . '/assets');
-        $this->setupAssets('js', ['js/sl-grid']);
-        $this->setupAssets('css', ['js/css-grid']);
-        parent::init();
-    }
+    public $sourcePath= __DIR__ . '/assets';
+    public $css= [
+        'css/css-grid',
+    ];
+    public $js=[
+        'js/js-grid',
+    ];
+
 }
