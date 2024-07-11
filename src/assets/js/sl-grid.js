1473cac9ffdc7d9ed4391bf9195308c9855db143
Samuele <46517478+SamueleLonghin@users.noreply.github.com>
2024-07-11 10:16:38 +0200

    Update sl-grid.js

--- src/assets/js/sl-grid.js
+++ src/assets/js/sl-grid.js
@@ -6,24 +6,11 @@ function cambiaPagina(e, url) {
 }
 
 
- // Salva l\'ID del tab attivo nel localStorage quando viene cambiato
-    $("a[data-toggle=\'tab\']").on("shown.bs.tab", function (e) {
-        var tabId = $(e.target).attr("href").substr(1);
-        localStorage.setItem("activeTab", tabId);
-        window.location.hash = tabId;
-    });
-
-    // Recupera l\'ID del tab attivo dal localStorage quando la pagina viene caricata
-    var activeTab = localStorage.getItem("activeTab");
-    if (activeTab) {
-        $("#myTab a[href=\'#" + activeTab + "\']").tab("show");
-    }
-
     // Intercetta i clic sui link di ordinamento della GridView e aggiungi il parametro del tab corrente all\'URL
-    $(document).on("click", ".sml-table-header th a[data-sort]", function (e) {
-        e.preventDefault();
-        var tabId = window.location.hash.substr(1);
-        var url = new URL($(this).attr("href"), window.location.origin);
-        url.hash = tabId;
-        window.location.href = url.toString();
-    });
+$(document).on("click", ".sml-table-header th a[data-sort]", function (e) {
+    e.preventDefault();
+    var tabId = window.location.hash.substr(1);
+    var url = new URL($(this).attr("href"), window.location.origin);
+    url.hash = tabId;
+    window.location.href = url.toString();
+});
