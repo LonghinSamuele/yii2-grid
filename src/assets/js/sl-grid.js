2cbc4c7b67385ae8b9874f90c9eb206cb01a5f58
Samuele <46517478+SamueleLonghin@users.noreply.github.com>
2024-07-17 12:05:22 +0200

    RowClick fixing

--- src/assets/js/sl-grid.js
+++ src/assets/js/sl-grid.js
@@ -1,4 +1,4 @@
-function cambiaPagina(e, url) {
+/*function cambiaPagina(e, url) {
     let t = $(e.target)[0];
     if (!$(t).is('A') && !$(t).is('SPAN') && !$(t).is('DIV') && !$(t).is('I') && !$(t).is('BUTTON') && !$(t).is('INPUT') && !$(t).is('TEXTAREA')) {
         window.location.href = url;
@@ -13,4 +13,21 @@ $(document).on("click", ".sml-table-header th a[data-sort]", function (e) {
     var url = new URL($(this).attr("href"), window.location.origin);
     url.hash = tabId;
     window.location.href = url.toString();
+});
+
+*/
+
+$(document).ready(function () {
+    $('.sl-gridview-clickable-row').each(function () {
+        const url = $(this).data('url');
+        $(this).find('td').each(function () {
+            // Controlla se la cella contiene già un link
+            const existingLink = $(this).find('a, button, input, select, textarea');
+            if (existingLink.length === 0) {
+                $(this).wrapInner('<a href=\"' + url + '\" class=\"stretched-link\"></a>');
+            } else {
+                existingLink.addClass('sl-gridview-existing-link');
+            }
+        });
+    });
 });
